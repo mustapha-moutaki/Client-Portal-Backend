88c3d56fdb1b476eefaba93e2e1cbf8ba7f44388
mustapha <mustaphaamoutaki@gmail.com>
2025-11-30 20:04:06 +0100

    feat: add base entity to avoid dry

---
+++ src/main/java/org/mustaha/ClientPortal/model/BaseEntity.java
@@ -0,0 +1,28 @@
+package org.mustaha.ClientPortal.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
