17fb826a6ce2cb892f043cde49c104bf9b300376
mustapha <mustaphaamoutaki@gmail.com>
2025-11-30 17:36:44 +0100

    inital commit

---
+++ src/main/java/org/mustaha/ClientPortal/ClientPortalApplication.java
@@ -0,0 +1,13 @@
+package org.mustaha.ClientPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mustaha/ClientPortal/ClientPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mustaha.ClientPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
